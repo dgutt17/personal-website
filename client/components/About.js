277fdab92ceecc7b93853ff5d4bd3d4bc74a27bd
Dan Gutt <dgutt10@gmail.com>
2018-08-17 16:39:22 -0500

    added animations to contacts styling to about and can switch between profile pics in about

--- client/components/About.js
+++ client/components/About.js
@@ -6,45 +6,67 @@ import {addAboutThunk} from '../store'
 export default class About extends Component {
     constructor (){
         super();
-        this.state ={}
+        this.state ={
+            currentPic: 2,
+            profileImg: '/img/profile.jpg',
+            profileOne: '/img/profile.jpg',
+            profileTwo: '/img/profile2.png',
+            profileThree: '/img/profile3.jpg'
+        }
     }
 
-    // componentDidMount(){
-    //     this.props.displayAbout()
-    // }
+    handleClick = () => {
+        if(this.state.currentPic < 3){
+            this.setState({
+               currentPic: this.state.currentPic + 1 
+            })
+        } else {
+            this.setState({
+                currentPic:1
+            })
+        }
+
+        if(this.state.currentPic === 1) {
+            this.setState({
+                profileImg: this.state.profileOne
+            })
+        }
+        else if(this.state.currentPic === 2) {
+            this.setState({
+                profileImg: this.state.profileTwo
+            })
+        }
+        else if(this.state.currentPic === 3) {
+            this.setState({
+                profileImg: this.state.profileThree
+            })
+        }
+
+    }
 
     render(){
+        console.log('currentPic:', this.state.currentPic)
         return (
             <div className="aboutWrapper">
-                <img className="profileImg" src='/img/profile.jpg'/>
+                <img className="profileImg" onClick={this.handleClick} src={this.state.profileImg}/>
                 <div className="aboutParagraph">
                     <p>
-                    I am a former attorney turned software engineer.
+                    <div className='aboutTitle'>MY STORY</div>
+                    <div>I am a former attorney turned software engineer.
                     One day I realized my favorite part of the day was my commute, and that certainly was not because of my 1 hour and 15 minute train ride. 
                     It was because I had started to teach myself how to code on my commute. 
                     I plunged into the world of software development first by teaching myself. 
                     Then I enrolled at Fullstack Academy of Code to further my training. 
                     I am interested in fullstack or backend work, and I have made several fullstack JavaScript applications. 
-                    As of right now, I am currently working on a movie recommendation engine.
+                    As of right now, I am currently working on a movie recommendation engine.</div>
+                    </p>
+                    <p>
+                    <div className='aboutTitle'>TECHNICAL SKILLS</div>
+                    <div>Proficient: JavaScript, Node.js, Express, React, Sequelize, Redux, Git, Github</div>
+                    <div>Knowledgeable: React Native, PostgreSQL, Mocha, Chai, Phaser.js, HTML, CSS, Python</div>
                     </p>
                 </div>
             </div>
         )
     }
 }
-
-
-// const mapStateToProps = state => {
-//     return {
-//         newAbout: state.newAbout
-//     }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         displayAbout: () => {
-//             dispatch(addAboutThunk())
-//         }
-//     }
-// }
-// export default connect(mapStateToProps, mapDispatchToProps)(About)
--- client/components/Contact.js
+++ client/components/Contact.js
@@ -10,9 +10,9 @@ export default class Portfolio extends Component {
         return (
             <div id='contactWrapper'>
                 <div id='contactTitle'>Contact Info</div>
-                <div className='contactItem'>Linkedin: <a href='linkedin.com/in/danielgutt/'>linkedin.com/in/danielgutt/</a></div>
-                <div className='contactItem'>Github: <a href='github.com/dgutt17'>github.com/dgutt17</a></div>
-                <div className='contactItem'>Email: dgutt10@gmail.com</div>
+                <div className='contactItem'>Linkedin: <a href='https://linkedin.com/in/danielgutt/'>linkedin.com/in/danielgutt/</a></div>
+                <div className='contactItem'>Github: <a href='https://github.com/dgutt17'>github.com/dgutt17</a></div>
+                <div className='contactItem'>Email: <p>dgutt10@gmail.com</p></div>
             </div> 
         )
     }
