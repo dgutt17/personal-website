96ccab3b77101467e12e8eb1679460c6864b3ab0
Dan Gutt <dgutt10@gmail.com>
2018-08-20 09:36:33 -0500

    navbar works

--- client/components/About.js
+++ client/components/About.js
@@ -7,15 +7,19 @@ export default class About extends Component {
     constructor() {
         super();
         this.state = {
-            currentPic: 2,
+            currentPic: 1,
             profileImg: '/img/profile.jpg',
             profileOne: '/img/profile.jpg',
             profileTwo: '/img/profile2.png',
-            profileThree: '/img/profile3.jpg'
+            profileThree: '/img/profile3.jpg',
+            
         }
     }
 
+
+    
     handleClick = () => {
+        console.log('before: ', this.state.currentPic)
         if (this.state.currentPic < 3) {
             this.setState({
                 currentPic: this.state.currentPic + 1
@@ -26,17 +30,22 @@ export default class About extends Component {
             })
         }
 
-        if (this.state.currentPic === 1) {
+        let currentPhoto = this.state.currentPic + 1
+        if(currentPhoto === 4){
+            currentPhoto = 1
+        }
+        
+        if (currentPhoto === 1) {
             this.setState({
                 profileImg: this.state.profileOne
             })
         }
-        else if (this.state.currentPic === 2) {
+        if (currentPhoto === 2) {
             this.setState({
                 profileImg: this.state.profileTwo
             })
         }
-        else if (this.state.currentPic === 3) {
+        if (currentPhoto === 3) {
             this.setState({
                 profileImg: this.state.profileThree
             })
@@ -45,58 +54,63 @@ export default class About extends Component {
     }
 
     render() {
-        console.log('currentPic:', this.state.currentPic)
+
         return (
             <div className="aboutWrapper">
                 <img className="profileImg" onClick={this.handleClick} src={this.state.profileImg} />
+                <div className='navdot'>
+                    <span id={this.state.currentPic === 1 ? 'clickedNavDot' : ''} onClick={() => this.setState({currentPic: 1, profileImg: this.state.profileOne})}></span>
+                    <span id={this.state.currentPic === 2 ? 'clickedNavDot' : ''} onClick={() => this.setState({currentPic: 2, profileImg: this.state.profileTwo})}></span>
+                    <span id={this.state.currentPic === 3 ? 'clickedNavDot' : ''} onClick={() => this.setState({currentPic: 3, profileImg: this.state.profileThree})}></span>
+                </div>
                 <div className="aboutParagraph">
-                    <div className='aboutSection1'>
-                        <div className='aboutTitle'>MY STORY</div>
-                        <p>I am a former attorney turned software engineer.
-                        One day I realized my favorite part of the day was my commute, and that certainly was not because of my 1 hour and 15 minute train ride.
-                        It was because I had started to teach myself how to code on my commute.
-                        I plunged into the world of software development first by teaching myself.
-                        Then I enrolled at Fullstack Academy of Code to further my training.
-                        I am interested in fullstack or backend work, and I have made several fullstack JavaScript applications.
-                    As of right now, I am currently working on a movie recommendation engine.</p>
-                    </div>
-                    <div className='aboutSection2'>
-                        <div className='aboutTitle'>TECHNICAL SKILLS</div>
-                        <div className='techSkills'>
-                            <div className='proficient'>
-                                <div>Proficient:</div>
-                                <div>
-                                    <ul>
-                                        <li>JavaScript</li>
-                                        <li>React</li>
-                                        <li>Redux</li>
-                                        <li>Node.js</li>
-                                        <li>Express</li>
-                                        <li>Sequelize</li>
-                                        <li>Git</li>
-                                        <li>Github</li>
-                                    </ul>
-                                </div>
+                <div className='aboutSection1'>
+                    <div className='aboutTitle'>MY STORY</div>
+                    <p>I am a former attorney turned software engineer.
+                    One day I realized my favorite part of the day was my commute, and that certainly was not because of my 1 hour and 15 minute train ride.
+                    It was because I had started to teach myself how to code on my commute.
+                    I plunged into the world of software development first by teaching myself.
+                    Then I enrolled at Fullstack Academy of Code to further my training.
+                    I am interested in fullstack or backend work, and I have made several fullstack JavaScript applications.
+                As of right now, I am currently working on a movie recommendation engine.</p>
+                </div>
+                <div className='aboutSection2'>
+                    <div className='aboutTitle'>TECHNICAL SKILLS</div>
+                    <div className='techSkills'>
+                        <div className='proficient'>
+                            <div>Proficient:</div>
+                            <div>
+                                <ul>
+                                    <li>JavaScript</li>
+                                    <li>React</li>
+                                    <li>Redux</li>
+                                    <li>Node.js</li>
+                                    <li>Express</li>
+                                    <li>Sequelize</li>
+                                    <li>Git</li>
+                                    <li>Github</li>
+                                </ul>
                             </div>
-                            <div className='knowledgable'>
-                                <div>Knowledgeable:</div>
-                                <div>
-                                    <ul>
-                                        <li>PostgreSQL</li>
-                                        <li>React Native</li>
-                                        <li>Mocha</li>
-                                        <li>Chai</li>
-                                        <li>Phaser.js</li>
-                                        <li>CSS</li>
-                                        <li>HTML</li>
-                                        <li>Python</li>
-                                    </ul>
-                                </div>
+                        </div>
+                        <div className='knowledgable'>
+                            <div>Knowledgeable:</div>
+                            <div>
+                                <ul>
+                                    <li>PostgreSQL</li>
+                                    <li>React Native</li>
+                                    <li>Mocha</li>
+                                    <li>Chai</li>
+                                    <li>Phaser.js</li>
+                                    <li>CSS</li>
+                                    <li>HTML</li>
+                                    <li>Python</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         )
     }
 }
