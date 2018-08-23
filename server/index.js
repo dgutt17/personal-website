c49364575b585ae408e901589066959cf7f15ba4
Dan Gutt <dgutt10@gmail.com>
2018-08-23 10:32:44 -0500

    change in server from wildcard to slash

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
-app.use('*', (req, res) => {
+app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
 
