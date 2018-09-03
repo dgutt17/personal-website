4334a06a18d4f00a5c7a63efce5e05972a6c59bd
Dan Gutt <dgutt10@gmail.com>
2018-09-03 14:22:01 -0500

    removed contact from navbar and added email icon to navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -17,11 +17,12 @@ export default class NavBar extends Component {
                         <li id='listItem'><Link to={'/'}>Home</Link></li>
                         <li id='listItem'><Link to={'/about'}>About</Link></li>
                         <li id='listItem'><Link to={'/portfolio'}>Portfolio</Link></li>
-                        <li id='listItem'><Link to={'/contact'}>Contact</Link></li>
+                        {/* <li id='listItem'><Link to={'/contact'}>Contact</Link></li> */}
                     </ul>
                     <ul id='social'>
-                        <li id='listItem' className='github'><a href="https://github.com/dgutt17">Github</a></li>
-                        <li id='listItem' className='linkedin'><a href="https://www.linkedin.com/in/danielgutt/">LinkedIn</a></li>
+                        <li id='listItemSocial' className='email'><a href="mailto:dgutt10@gmail.com">Email</a></li>
+                        <li id='listItemSocial' className='github'><a href="https://github.com/dgutt17">Github</a></li>
+                        <li id='listItemSocial' className='linkedin'><a href="https://www.linkedin.com/in/danielgutt/">LinkedIn</a></li>
                     </ul>
                 </nav>
             </div>
